2d3d939c466a87e4b42c7898407cff3e28e081bd
Jonathan <jonathan24.dp@gmail.com>
2022-08-08 23:16:47 -0500

    diseño casi terminado

--- assets/js/app.js
+++ assets/js/app.js
@@ -110,55 +110,55 @@ jQuery(document).ready(function ($) {
 
 // Add active class on another page linked
 // ==========================================
-// $(window).on('load',function () {
-//     var current = location.pathname;
-//     console.log(current);
-//     $('#navbarSupportedContent ul li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.parent().addClass('active');
-//             $this.parents('.menu-submenu').addClass('show-dropdown');
-//             $this.parents('.menu-submenu').parent().addClass('active');
-//         }else{
-//             $this.parent().removeClass('active');
-//         }
-//     })
-// });
+$(window).on('load', function () {
+    var current = location.pathname;
+    console.log(current);
+    $('#navbarSupportedContent ul li a').each(function () {
+        var $this = $(this);
+        // if the current path is like this link, make it active
+        if ($this.attr('href').indexOf(current) !== -1) {
+            $this.parent().addClass('active');
+            $this.parents('.menu-submenu').addClass('show-dropdown');
+            $this.parents('.menu-submenu').parent().addClass('active');
+        } else {
+            $this.parent().removeClass('active');
+        }
+    })
+});
 
 /* Writing */
-$(function(){
-	$(".typed").typed({
-		strings: ["Jugar Video Juegos.", "El Ajedrez.", "El Trading.", "Programar."],
-		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
-		stringsElement: null,
-		// typing speed
-		typeSpeed: 30,
-		// time before typing starts
-		startDelay: 1200,
-		// backspacing speed
-		backSpeed: 20,
-		// time before backspacing
-		backDelay: 500,
-		// loop
-		loop: true,
-		// false = infinite
-		loopCount: 5,
-		// show cursor
-		showCursor: false,
-		// character for cursor
-		cursorChar: "|",
-		// attribute to type (null == text)
-		attr: null,
-		// either html or text
-		contentType: 'html',
-		// call when done callback function
-		callback: function() {},
-		// starting callback function before each string
-		preStringTyped: function() {},
-		//callback for every typed string
-		onStringTyped: function() {},
-		// callback for reset
-		resetCallback: function() {}
-	});
+$(function () {
+    $(".typed").typed({
+        strings: ["Jugar Video Juegos.", "El Ajedrez.", "El Trading.", "Programar."],
+        // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+        stringsElement: null,
+        // typing speed
+        typeSpeed: 30,
+        // time before typing starts
+        startDelay: 1200,
+        // backspacing speed
+        backSpeed: 20,
+        // time before backspacing
+        backDelay: 500,
+        // loop
+        loop: true,
+        // false = infinite
+        loopCount: 5,
+        // show cursor
+        showCursor: false,
+        // character for cursor
+        cursorChar: "|",
+        // attribute to type (null == text)
+        attr: null,
+        // either html or text
+        contentType: 'html',
+        // call when done callback function
+        callback: function () { },
+        // starting callback function before each string
+        preStringTyped: function () { },
+        //callback for every typed string
+        onStringTyped: function () { },
+        // callback for reset
+        resetCallback: function () { }
+    });
 });
--- assets/js/app2.js
+++ assets/js/app2.js
@@ -1,39 +1,35 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Transition effect for navbar and back-to-top icon
-    $(window).scroll(function() {
-      // checks if window is scrolled more than 500px, adds/removes solid class
-      if($(this).scrollTop() > 550) { 
-          $('.navbar').addClass('solid');
-          $('.back-to-top').addClass('visible'); 
-      } else {
-          $('.navbar').removeClass('solid');
-          $('.back-to-top').removeClass('visible');  
-      }
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
 
     });
 
 
     // Scrolling effect for Arrow icons
-    $("#scrollIcon").click(function(e) {
+    $("#scrollIcon").click(function (e) {
         e.preventDefault();
-        $.scrollTo($("#about"), 1000);
+        $.scrollTo($("#about"), 500);
     });
-    $("#nav-about").click(function(e) {
+    $(".navbar-brand").click(function (e) {
         e.preventDefault();
-        $.scrollTo($("#about"), 1000);
+        $.scrollTo(0, 500);
     });
-    $("#nav-portfolio").click(function(e) {
+    $("#scrollProjects").click(function (e) {
         e.preventDefault();
-        $.scrollTo($("#portfolio"), 1000);
+        $.scrollTo($("#portfolio"), 500);
     });
-    $("#nav-contact").click(function(e) {
+    $("#scrollPortfolio").click(function (e) {
         e.preventDefault();
-        $.scrollTo($("#contact"), 1000);
+        $.scrollTo($("#portfolio"), 500);
     });
-    $(".navbar-brand").click(function(e) {
-        e.preventDefault();
-        $.scrollTo(0, 1000);
-    });
-      
-  });
+
+});
