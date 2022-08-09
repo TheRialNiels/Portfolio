01bd44236d8d5f82e02797e7a561bdfff6edbc5d
Jonathan <jonathan24.dp@gmail.com>
2022-08-09 14:13:05 -0500

    codigo comentado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,57 +1,8 @@
-function EnviarCorreo() {
-    var nombre = $('#name').val();
-    var email = $('#email').val();
-    var asunto = $('#subject').val();
-    var mensaje = $('#message').val();
-
-    if (nombre != "" && email != "" && asunto != "" && mensaje != "") {
-        $.ajax({
-            url: 'https://formsubmit.co/ajax/41aa9415c1a6a44e10be761b5f78a569',
-            type: 'POST',
-            dataType: 'json',
-            accepts: 'application/json',
-            /* data: { nombre: nombre, email: email, asunto: asunto, mensaje: mensaje }, */
-            data: {
-
-                name: nombre,
-                email: email,
-                subject: asunto,
-                message: mensaje
-            },
-            async: true,
-            success: function (datos) {
-                if (datos.success == "true") {
-                    swal({
-                        title: "Exito",
-                        text: "Se ha enviado el correo con exito",
-                        type: "success",
-                        showCancelButton: false,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "OK",
-                        closeOnConfirm: true,
-                        closeOnCancel: false
-                    }, function (isConfirm) {
-                        location.href = "";
-                    });
-                } else {
-                    swal({
-                        title: "Error",
-                        text: datos.message,
-                        type: "error",
-                        showCancelButton: false,
-                        confirmButtonColor: "#DD6B55",
-                        confirmButtonText: "Cerrar",
-                        closeOnConfirm: true,
-                        closeOnCancel: false
-                    });
-                }
-            }
-        });
-    } else {
-        swal("Cuidado", "Aun existen campos vacios", "warning");
-    }
-}
-
+/* 
+    ######################################################
+    ##################  Navbar Section  ##################
+    ######################################################
+*/
 // ---------Responsive-navbar-active-animation-----------
 function test() {
     var tabsNewAnim = $('#navbarSupportedContent');
@@ -126,6 +77,52 @@ $(window).on('load', function () {
     })
 });
 
+$(document).ready(function () {
+
+    // Transition effect for navbar and back-to-top icon
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 550) {
+            $('.navbar').addClass('solid');
+            $('.back-to-top').addClass('visible');
+        } else {
+            $('.navbar').removeClass('solid');
+            $('.back-to-top').removeClass('visible');
+        }
+
+    });
+
+
+    // Scrolling effect for Arrow icons
+    $("#scrollIcon").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 500);
+    });
+    $(".navbar-brand").click(function (e) {
+        e.preventDefault();
+        $.scrollTo(0, 500);
+    });
+    $("#scrollProjects").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 500);
+    });
+    $("#scrollPortfolio").click(function (e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 500);
+    });
+
+});
+
+/* 
+    ######################################################
+    ###################  Home Section  ###################
+    ######################################################
+*/
+/* 
+    ######################################################
+    ##################  About Section  ###################
+    ######################################################
+*/
 /* Writing */
 $(function () {
     $(".typed").typed({
@@ -162,3 +159,67 @@ $(function () {
         resetCallback: function () { }
     });
 });
+
+/* 
+    ######################################################
+    ################  Portfolio Section  #################
+    ######################################################
+*/
+/* 
+    ######################################################
+    #################  Contact Section  ##################
+    ######################################################
+*/
+function EnviarCorreo() {
+    var nombre = $('#name').val();
+    var email = $('#email').val();
+    var asunto = $('#subject').val();
+    var mensaje = $('#message').val();
+
+    if (nombre != "" && email != "" && asunto != "" && mensaje != "") {
+        $.ajax({
+            url: 'https://formsubmit.co/ajax/41aa9415c1a6a44e10be761b5f78a569',
+            type: 'POST',
+            dataType: 'json',
+            accepts: 'application/json',
+            /* data: { nombre: nombre, email: email, asunto: asunto, mensaje: mensaje }, */
+            data: {
+
+                name: nombre,
+                email: email,
+                subject: asunto,
+                message: mensaje
+            },
+            async: true,
+            success: function (datos) {
+                if (datos.success == "true") {
+                    swal({
+                        title: "Exito",
+                        text: "Se ha enviado el correo con exito",
+                        type: "success",
+                        showCancelButton: false,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "OK",
+                        closeOnConfirm: true,
+                        closeOnCancel: false
+                    }, function (isConfirm) {
+                        location.href = "";
+                    });
+                } else {
+                    swal({
+                        title: "Error",
+                        text: datos.message,
+                        type: "error",
+                        showCancelButton: false,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Cerrar",
+                        closeOnConfirm: true,
+                        closeOnCancel: false
+                    });
+                }
+            }
+        });
+    } else {
+        swal("Cuidado", "Aun existen campos vacios", "warning");
+    }
+}
